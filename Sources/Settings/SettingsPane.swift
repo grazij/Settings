a8dc28ab36698783f8d62da7039f6c64ed2098e5
Jose Graziani <josegraziani@gmail.com>
2025-12-16 18:16:38 -0500

    Add documentation for SettingsPane protocol
    Document the protocol and its required properties with DocC
    comments, including a usage example.

--- Sources/Settings/SettingsPane.swift
+++ Sources/Settings/SettingsPane.swift
@@ -10,9 +10,37 @@ extension Settings {
 	}
 }
 
+/**
+A protocol that view controllers must conform to in order to be used as a settings pane.
+
+Implement this protocol on your `NSViewController` subclass to create a pane that can be displayed in the settings window.
+
+```swift
+final class GeneralSettingsViewController: NSViewController, SettingsPane {
+    let paneIdentifier = Settings.PaneIdentifier("general")
+    let paneTitle = "General"
+    let toolbarItemIcon = NSImage(systemSymbolName: "gearshape", accessibilityDescription: "General settings")!
+}
+```
+*/
 public protocol SettingsPane: NSViewController {
+	/**
+	A unique identifier for this pane.
+
+	Used for state restoration and programmatic pane switching.
+	*/
 	var paneIdentifier: Settings.PaneIdentifier { get }
+
+	/**
+	The title displayed in the toolbar and window title.
+	*/
 	var paneTitle: String { get }
+
+	/**
+	The icon displayed in the toolbar.
+
+	Defaults to an empty image if not implemented.
+	*/
 	var toolbarItemIcon: NSImage { get }
 }
 
